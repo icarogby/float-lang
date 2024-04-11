280053584f0aa0d8f575ef9b76732491e6a7fbb8
Ícaro Gabryel <91147230+icarogby@users.noreply.github.com>
2024-04-11 00:02:24 -0300

    ?

--- compiler/flooatParser.py
+++ compiler/flooatParser.py
@@ -6,9 +6,12 @@ class Parser:
         self.symbolTable = []
         self.pythonCode = ''
         self.index = -1
-        self.bkpIndex = 0
-        self.tempMsgError = ''
+        self.expectedTokens = []
+        self.SyntaxErrorMsg = f'Syntax error: Expected {list(set(self.expectedTokens))}.\n Found: {self.getCurrentToken()[0]}'
 
+    def getCurrentToken(self):
+        return self.tokens[self.index]
+    
     def getNextToken(self):
         self.index += 1
         return self.tokens[self.index]
@@ -18,7 +21,15 @@ class Parser:
     
     def matchToken(self, token: str):
         if self.getNextToken()[0] != token:
-            raise Exception(f'{self.tempMsgError}Syntax error: Expected "{token}"')
+            self.expectedTokens.append(token)
+            raise Exception(self.SyntaxErrorMsg)
+        
+    def tryEnter(self, func, firstSet):
+        if self.peekNextToken()[0] in firstSet:
+            func()
+        else:
+            for token in firstSet:
+                self.expectedTokens.append(token)
 
     def init(self):
         self.comp()
@@ -29,14 +40,12 @@ class Parser:
         self.matchToken('id')
         self.symbolTable.append(self.tokens[self.index][1])
         
-        if self.getNextToken()[1] != '(':
-            raise Exception(f'{self.tempMsgError}Syntax error: Expected "("')
+        self.matchToken('[')
         
-        if self.peekNextToken()[1] == 'bit':
-            self.parameters()
-        elif self.peekNextToken()[1] != ')':
-            raise Exception(f'{self.tempMsgError}Syntax error: Expected "bit" or ")"')
+        self.tryEnter(self.parameters(), ['bit'])
 
+        self.matchToken(']')
+        
         if self.getNextToken()[1] != ')':
             raise Exception(f'{self.tempMsgError}Syntax error: Expected ")"')
         
